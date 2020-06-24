1bab2daea476e1a50f216ec2546c8fa4f0347da1
Tommalieh <muhammedtommalieh@gmail.com>
2020-06-24 11:09:34 +0300

    Added ACL

--- src/auth-middleware/authorize.js
+++ src/auth-middleware/authorize.js
@@ -7,8 +7,9 @@
 
 module.exports = (capability) => {
   return (req, res, next) => {
+    console.log(req.user.capabilities);
     try {
-      if (req.user.capabilities.includes(capability)) {
+      if (req.user.capabilities.capabilities.includes(capability)) {
         next();
       } else {
         next('Access Denied!!');
